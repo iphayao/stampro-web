85f354e65dc57997e7d6ba9cc4e9015a02e8b1b7
Phayao Boonon <phayaob@gmail.com>
2017-06-23 22:41:41 +0700

    Update webapp Toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+/*@Component({
+  selector: 'tabs-overview-example',
+  templateUrl: './tabs-overview-example.html'
+})
+ 
+export class TabComponent { }*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Stampro';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
