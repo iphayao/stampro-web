b2d86dda652c862089c5119e118e88c67a1b65d1
Phayao Boonon <phayaob@gmail.com>
2017-06-24 08:54:57 +0700

    Add Angularfire2 for authen and database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
-
-/*@Component({
-  selector: 'tabs-overview-example',
-  templateUrl: './tabs-overview-example.html'
-})
- 
-export class TabComponent { }*/
+import { MdDialog } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'Stampro';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  constructor(public db: AngularFireDatabase, public af: AngularFireAuth, public dlg: MdDialog) {
+    this.items = db.list('/items');
+    this.user = af.authState;
+  }
+
+  openDialog() {
+    this.dlg.open(AppDialog)
+  }
 }
+
+@Component({
+  templateUrl: './app.component.dialog.html',
+})
+
+export class AppDialog {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+//import * as firebase from 'firebase/app';
+//import { firebase } from 'firebase/app';
 
-import { AppComponent } from './app.component';
+import { AppComponent, AppDialog } from './app.component';
+
 import 'hammerjs';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBFPr0oGKWjlu_xfV2pJAHwlU1ixLaKHDk",
+    authDomain: "stampro-6f860.firebaseapp.com",
+    databaseURL: "https://stampro-6f860.firebaseio.com",
+    projectId: "stampro-6f860",
+    storageBucket: "stampro-6f860.appspot.com",
+    messagingSenderId: "771235130895"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppDialog,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
-  
+  entryComponents: [AppDialog],
   providers: [],
   bootstrap: [AppComponent]
 })
