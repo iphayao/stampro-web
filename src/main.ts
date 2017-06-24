21f95c835fd20d17f7c7e6b521d1df3270d44eb5
Phayao Boonon <phayaob@gmail.com>
2017-06-24 10:21:11 +0700

    Add simple Service Worker for offline cache

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,12 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => console.log('Service Worker registration successful with scope: ', registration.scope));
+    }
+  })
+  .catch(err => console.error(err));
